5093b6d1d8e38da76bebc9c541c47d89f69f9d8e
Gleidson <Gleidbrianmuniz@gmail.com>
2025-09-29 16:23:27 -0300

    Etapa 4 - Atualização de disponibilidade

--- funcoes.py
+++ funcoes.py
@@ -13,5 +13,35 @@ def listar_livro():
     cursor = conexao.cursor()
     cursor.execute("SELECT * FROM Biblioteca")
 #fetchall traz todas as linhas da consulta
-    for linha in cursor.fetchall(): return(f"ID {linha[0]}| Título: {linha[1]} | Ano: {linha[2]} | Autor: {linha[3]} | Disponivel : {linha[4]}")
+    for linha in cursor.fetchall(): 
+        return(f"ID {linha[0]}| Título: {linha[1]} | Ano: {linha[2]} | Autor: {linha[3]} | Disponivel : {linha[4]}")
 
+def updade_dispo(id_livro):
+    conexao = sqlite3.connect("Biblioteca.db")
+    cursor = conexao.cursor()
+
+    cursor.execute("SELECT disponivel FROM Biblioteca WHERE id = ?", (id_livro,))
+    resultado = cursor.fetchone()
+
+    if resultado:
+        status_atual = resultado[0]
+
+        if status_atual.lower() == "sim":
+            novo_status = "Não"
+        else:
+            novo_status = "Sim"
+
+        cursor.execute("""
+            UPDATE Biblioteca
+            SET disponivel = ?
+            WHERE id = ?
+        """, (novo_status, id_livro))
+
+        conexao.commit()
+        print(f"Status atualizado para '{novo_status}'.")
+    else:
+        print("Livro com o ID informado não encontrado.")
+
+    conexao.close()
+
+    
--- main.py
+++ main.py
@@ -4,20 +4,22 @@ conexao = sqlite3.connect("Biblioteca.db")
 cursor = conexao.cursor()
 
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Biblioteca(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        titulo TEXT NOT NULL,  
-        autor TEXT NOT NULL,
-        ano INTEGER,
-         disponivel TEXT)
-     """)
-conexao.commit()
-conexao.close()
-titulo = input("Digite o titulo do livro que deseja cadastrar: ")
-autor = input("Digite o autor do livro: ")
-ano = int(input("Insirar o ano do livro: "))
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS Biblioteca(
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         titulo TEXT NOT NULL,  
+#         autor TEXT NOT NULL,
+#         ano INTEGER,
+#          disponivel TEXT)
+#      """)
+# conexao.commit()
+# conexao.close()
+# titulo = input("Digite o titulo do livro que deseja cadastrar: ")
+# autor = input("Digite o autor do livro: ")
+# ano = int(input("Insirar o ano do livro: "))
 
 
-funcoes.cadastrar_livro(titulo,autor,ano)
-print(funcoes.listar_livro())
+# funcoes.cadastrar_livro(titulo,autor,ano)
+# print(funcoes.listar_livro())
+id_disponivel = int(input("Digite o id do livro alterado: "))
+funcoes.updade_dispo(id_disponivel)
